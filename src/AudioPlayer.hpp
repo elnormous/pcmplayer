25ead35779e35c146c003fe7c6d80e1e975d139c
Elviss Strazdins <elviss@elviss.lv>
2020-10-30 09:58:46 +0200

    Get rid of AudioPlayer.cpp

--- src/AudioPlayer.hpp
+++ src/AudioPlayer.hpp
@@ -16,16 +16,56 @@ namespace pcmplayer
                     std::uint32_t initBufferSize,
                     std::uint32_t initSampleRate,
                     SampleFormat initSampleFormat,
-                    std::uint16_t initChannels);
+                    std::uint16_t initChannels) :
+            driver(initDriver),
+            bufferSize(initBufferSize),
+            sampleRate(initSampleRate),
+            sampleFormat(initSampleFormat),
+            channels(initChannels)
+        {
+        }
+
         virtual ~AudioPlayer() = default;
 
-        void play(const std::vector<float> s);
+        void play(const std::vector<float> s)
+        {
+            samples = s;
+            start();
+        }
 
     protected:
         virtual void start() = 0;
         virtual void stop() = 0;
 
-        bool getData(std::uint32_t frames, std::vector<float>& result);
+        bool getData(std::uint32_t frames, std::vector<float>& result)
+        {
+            result.clear();
+            result.reserve(frames * channels);
+
+            const std::size_t bufferFrames = samples.size() / channels;
+            const std::size_t remainingFrames = bufferFrames - offset;
+
+            if (remainingFrames > frames)
+            {
+                result.insert(result.end(),
+                              samples.begin() + offset * channels,
+                              samples.begin() + (offset + frames) * channels);
+
+                offset += frames;
+
+                return true;
+            }
+            else
+            {
+                result.insert(result.end(),
+                    samples.begin() + offset * channels,
+                    samples.end());
+
+                offset = bufferFrames;
+
+                return false;
+            }
+        }
 
         Driver driver;
 
